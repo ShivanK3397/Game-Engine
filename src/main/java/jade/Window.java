b38c0a8a4431f0c67c0249154edc00629d30f3fa
Shivan Kathir <shivan.kathir@gmail.com>
2026-09-25 19:34:58 -0400

    Added event listeners and a scene manager

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -4,7 +4,9 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import util.Time;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
 import static org.lwjgl.glfw.GLFW.*;
@@ -13,26 +15,61 @@ public class Window {
     private int width,height;
     private String title;
     private long glfwWindow;
-
+    private boolean fadeToBlack;
+    public float r,g,b,a;
     private static Window window = null;
+    private static Scene currentScene = null;
+
     private Window(){
         this.width=1080;
         this.height=600;
         this.title="Mario";
+        r=1;
+        g=1;
+        b=1;
+        a=1;
     }
 
+    public static void changeScene(int newScene){
+        switch(newScene){
+            case 0:
+                currentScene = new LevelEditorScene();
+                //currentScene.init();
+                break;
+
+            case 1:
+                currentScene = new LevelScene();
+                break;
+
+            default:
+                assert false: "Unknown scene'" + newScene + "'";
+                break;
+        }
+    }
     public static Window get(){
         if (Window.window == null){
             Window.window = new Window();
+
         }
         return Window.window;
     }
+
+
 
     public void run(){
         System.out.println("Hello LWJGL"+ Version.getVersion());
 
         init();
         loop();
+
+        //Free memory
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        //Terminate GLFW and free Error Callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+
     }
 
     public void init(){
@@ -52,10 +89,14 @@ public class Window {
 
         //Create the window
         glfwWindow = glfwCreateWindow( this.width, this.height,this.title, NULL,NULL);
-
         if(glfwWindow == NULL){
             throw new IllegalStateException("Failed to create the GLFW window");
         }
+
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
 
         //Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
@@ -72,17 +113,30 @@ public class Window {
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
         GL.createCapabilities();
+
+        window.changeScene(0);
     }
 
     public void loop(){
+        float beginTime = (float) glfwGetTime();
+        float endTime = (float)  glfwGetTime();
+        float dt  = -1.0f;
         while (!glfwWindowShouldClose(glfwWindow)){
             //Poll events
             glfwPollEvents();
 
-            glClearColor(1.0f,1.0f,1.0f,1.0f);
+            glClearColor(r,g,b,a);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            if(dt>=0) {
+                currentScene.update(dt);
+            }
+
             glfwSwapBuffers(glfwWindow);
+
+            endTime = (float) glfwGetTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,5 +9,5 @@ public class Main {
     static void main(String[] args) {
         Window window = Window.get();
         window.run();
-
+    }
 }
